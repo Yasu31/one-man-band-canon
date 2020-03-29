c63f45a0dd6581984c176c5131d6207681cc7105
Yasunori Toshimitsu <yasu@Yasunoris-MacBook-Pro.local>
2020-03-29 20:47:30 +0900

    add comments

--- script.py
+++ script.py
@@ -3,11 +3,11 @@ from scipy.io.wavfile import write
 import numpy as np
 import datetime
 
-segmentDuration = 10
-fs = 44100
-repetitions = 34
+segmentDuration = 10 # duration in seconds for 2 bars.
+fs = 44100 # sampling freq.
+repetitions = 34 # repeat a bit more than is necessary
 
-# construct the cello segment
+#### hardcode the cello part ####
 celloSegment = np.zeros((segmentDuration*fs,1))
 # D, A, B, F#, G, D, G, A
 # https://pages.mtu.edu/~suits/notefreqs.html
@@ -18,10 +18,14 @@ for i in range(len(noteFreqList)):
     celloNote = 0.3 * np.sin(2*np.pi*noteFreq*t)
     celloNote = celloNote.reshape((-1, 1))
     celloSegment[segmentDuration*fs*i//len(noteFreqList):segmentDuration*fs*(i+1)//len(noteFreqList), :] = celloNote
+    # by not having the note play all the time, I feel it becomes easier to feel the tempo
+    # So, cut the sound at 3/4 of the quarter note
     celloSegment[int(segmentDuration*fs*(i+0.75)//len(noteFreqList)):segmentDuration*fs*(i+1)//len(noteFreqList), :] = 0
-    # todo: smoother fade in / out
+    # todo: this is a bit too abrupt. smoother fade in / out
 
+#### Allocate array to store performance ####
 # "premature optimization is the root of all evil"
+# this uses a lot of memory, but that can be dealt with later when I feel like it. For the time being, this works.
 first = np.zeros((segmentDuration*fs*repetitions, 1))
 second = np.zeros((segmentDuration*fs*repetitions, 1))
 third = np.zeros((segmentDuration*fs*repetitions, 1))
@@ -34,12 +38,17 @@ def time2index(sdTime):
     return int((sdTime - initialSdTime) * fs)
 
 def callback(indata, outdata, frames, time, status):
-    if status:
-        print(status)
-    # save input to first violin part
+    '''manage sound I/O
+    callback for the Stream object that receives mic input and determines speaker output.
+    https://python-sounddevice.readthedocs.io/en/0.3.15/usage.html#callback-streams
+    '''
     global initialSdTime
     if initialSdTime is None:
         initialSdTime = time.inputBufferAdcTime
+    if status:
+        print(status)
+
+    # save input of first violin to respective positions in each part
     i = time2index(time.inputBufferAdcTime)
     first[i:i+frames, :] = indata
     i += segmentDuration * fs
@@ -47,14 +56,15 @@ def callback(indata, outdata, frames, time, status):
     i += segmentDuration * fs
     third[i:i+frames, :] = indata
 
-    # create accompaniment music
+    # create accompaniment music to output from speaker
     i = time2index(time.outputBufferDacTime)
     outdata[:] = cello[i:i+frames] + second[i:i+frames] + third[i:i+frames]
 
 try:
     with sounddevice.Stream(channels=1, samplerate=fs, callback=callback) as stream:
         input()
-except KeyboardInterrupt or ValueError:
+except (KeyboardInterrupt, ValueError):
+    # ctrl-c or recording overflows allocated array
     pass
 
 now = datetime.datetime.now()
