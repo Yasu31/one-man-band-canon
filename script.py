ce89709af7dd1efd44a6be23099636b57ddc9045
Yasunori Toshimitsu <yasu@Yasunoris-MacBook-Pro.local>
2020-03-29 14:34:29 +0900

    uses Stream for zero gaps!

--- script.py
+++ script.py
@@ -1,22 +1,11 @@
 import sounddevice
 from scipy.io.wavfile import write
 import numpy as np
-from threading import Timer
-from time import sleep, time
+import datetime
 
 segmentDuration = 8
 fs = 44100
-
-first = np.zeros((0,1))
-second = np.zeros((0,1))
-third = np.zeros((0,1))
-
-# what second and third player should be playing in current segment
-# respectively the previous and previous previous segments played by the first player
-secondHalfSegment1 = np.zeros((segmentDuration*fs//2,1))
-thirdHalfSegment1 = np.copy(secondHalfSegment1)
-secondHalfSegment2 = np.copy(secondHalfSegment1)
-thirdHalfSegment2 = np.copy(secondHalfSegment1)
+repetitions = 30
 
 # construct the cello segment
 celloSegment = np.zeros((segmentDuration*fs,1))
@@ -26,37 +15,49 @@ noteFreqList = [293.66, 220, 246.94, 185.00, 196, 146.83, 196, 220]
 for i in range(len(noteFreqList)):
     noteFreq = noteFreqList[i]
     t = np.arange(segmentDuration*fs/len(noteFreqList))/fs
-    celloNote = 0.1 * np.sin(2*np.pi*noteFreq*t)
+    celloNote = 0.05 * np.sin(2*np.pi*noteFreq*t)
     celloNote = celloNote.reshape((-1, 1))
     celloSegment[segmentDuration*fs*i//len(noteFreqList):segmentDuration*fs*(i+1)//len(noteFreqList), :] = celloNote
-celloHalfSegment1 = celloSegment[:segmentDuration*fs//2, :]
-celloHalfSegment2 = celloSegment[segmentDuration*fs//2:, :]
 
-wholeRecording = np.copy(celloHalfSegment1)
-def save(first, second, third, cello, accompaniment):
-    global wholeRecording
-    wholeRecording = np.append(wholeRecording, first+second+third+cello, axis=0)
-    third[:] = np.copy(second)
-    second[:] = np.copy(first)
-    accompaniment[:] = second + third + cello
+# "premature optimization is the root of all evil"
+first = np.zeros((segmentDuration*fs*repetitions, 1))
+second = np.zeros((segmentDuration*fs*repetitions, 1))
+third = np.zeros((segmentDuration*fs*repetitions, 1))
+cello = np.tile(celloSegment, (repetitions, 1))
 
-accompanimentHalfSegment1 = np.copy(celloHalfSegment1)
-accompanimentHalfSegment2 = np.copy(celloHalfSegment2)
-firstHalfSegment2 = np.zeros((segmentDuration*fs//2,1))
-timing = 0.3
+initialSdTime = None
+def time2index(sdTime):
+    '''convert time obtained from sounddevice into index of the data 
+    '''
+    return int((sdTime - initialSdTime) * fs)
+
+def callback(indata, outdata, frames, time, status):
+    if status:
+        print(status)
+    # save input to first violin part
+    global initialSdTime
+    if initialSdTime is None:
+        initialSdTime = time.inputBufferAdcTime
+    i = time2index(time.inputBufferAdcTime)
+    first[i:i+frames, :] = indata
+    i += segmentDuration * fs
+    second[i:i+frames, :] = indata
+    i += segmentDuration * fs
+    third[i:i+frames, :] = indata
+
+    # create accompaniment music
+    i = time2index(time.outputBufferDacTime)
+    print(i)
+    outdata[:] = cello[i:i+frames] + second[i:i+frames] + third[i:i+frames]
+
 try:
-    while True:
-        # first half-segment
-        firstHalfSegment1 = sounddevice.playrec(accompanimentHalfSegment1, samplerate=fs, channels=1)
-        t = Timer(segmentDuration/4, save, args=(firstHalfSegment2, secondHalfSegment2, thirdHalfSegment2, celloHalfSegment2, accompanimentHalfSegment2))
-        t.start()
-        sleep(segmentDuration/2-timing)
-
-        firstHalfSegment2 = sounddevice.playrec(accompanimentHalfSegment2, samplerate=fs, channels=1)
-        t = Timer(segmentDuration/4, save, args=(firstHalfSegment1, secondHalfSegment1, thirdHalfSegment1, celloHalfSegment1, accompanimentHalfSegment1))
-        t.start()
-        sleep(segmentDuration/2-timing)
-        
+    with sounddevice.Stream(channels=1, samplerate=fs, callback=callback) as stream:
+        input()
 except KeyboardInterrupt:
     pass
-write("output.wav", fs, wholeRecording)
+
+now = datetime.datetime.now()
+now.strftime("%Y/%m/%d %H:%M:%S")
+filename = "canon-{}.wav".format(datetime.datetime.now().strftime("%Y%m%d-%H-%M"))
+print("saving to {}".format(filename))
+write(filename, fs, first+second+third+cello)
